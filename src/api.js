a80d4e21e02b8f90ecb9828072770da5d6b678ea
Pujan <pujanshah825@gmail.com>
2024-06-09 23:50:23 +0530

    weather-app complete

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+	method: "GET",
+	headers: {
+	  "X-RapidAPI-Key": "42066b22c1msh9fe2d14c2949198p17fe9cjsn219587557ed8",
+	  "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+	},
+  };
+  export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+  export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "37581205838b20bf6e4983d435f700d1"; 
